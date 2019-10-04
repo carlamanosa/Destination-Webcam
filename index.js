16dca07902cf4cde55b082e85379491efcb2b685
Sean McClain <srmcclain@me.com>
2019-10-04 14:02:54 -0700

    Changed webacm api url parameters to reflect google maps long lat values.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // Function for displaying webcam feeds
 function displayWebcam() {
   // API URL
-  var queryURL = "https://webcamstravel.p.rapidapi.com/webcams/list/nearby=46.54,7.98,5?show=webcams:player";
+  var queryURL = "https://webcamstravel.p.rapidapi.com/webcams/list/nearby="+latitudeLoc+","+longitudeLoc+",50?show=webcams:player";
 
   // Requesting data from webcam.travel API
   $.ajax({
